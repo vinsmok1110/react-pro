e73f08c1948791554b5a7f216cb89e4d3d7b9b73
vinsmok1110 <vinsmok111@gmail.com>
2024-07-17 11:01:08 +0800

    HandsOnExerciseEmployeeRecords

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -2,60 +2,35 @@ import React from "react";
 
 const Employees = () => {
   const employees = [
-    {
-      name: "Gian R.",
-      position: "Manager",
-      salary: 99999,
-      status: "Active",
-    },
+    { name: "Gian R.", position: "Manager", salary: 20000, status: "Active" },
     {
       name: "Ernest Q.",
       position: "Developer",
-      salary: 123456,
+      salary: 15000,
       status: "Active",
     },
     {
       name: "Ryan L.",
       position: "Designer",
-      salary: 123123123,
+      salary: 50000,
       status: "Active",
     },
     {
       name: "Creshell A.",
       position: "Developer",
-      salary: 12398172398,
+      salary: 10000,
       status: "Active",
     },
-    {
-      name: "Ana L.",
-      position: "Manager",
-      salary: 90,
-      status: "Active",
-    },
-    {
-      name: "James Q.",
-      position: "Designer",
-      salary: 69,
-      status: "Active",
-    },
-    {
-      name: "Ky L.",
-      position: "Developer",
-      salary: 73,
-      status: "Active",
-    },
+    { name: "Ana L.", position: "Manager", salary: 90000, status: "Active" },
+    { name: "James Q.", position: "Designer", salary: 69000, status: "Active" },
+    { name: "Ky L.", position: "Developer", salary: 45000, status: "Active" },
     {
       name: "Maria L.",
       position: "Designer",
-      salary: 60,
+      salary: 650000,
       status: "Active",
     },
-    {
-      name: "Mary A.",
-      position: "Manager",
-      salary: 0,
-      status: "Not Active",
-    },
+    { name: "Mary A.", position: "Manager", salary: 0, status: "Not Active" },
     {
       name: "Heinrich R.",
       position: "Developer",
@@ -77,23 +52,16 @@ const Employees = () => {
       </thead>
       <tbody>
         {employees.map((employee, index) => (
-          <Employee key={index} index={index + 1} employee={employee} />
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{employee.name}</td>
+            <td>{employee.position}</td>
+            <td>{employee.status === "Active" ? employee.salary : "N/A"}</td>
+            <td>{employee.status}</td>
+          </tr>
         ))}
       </tbody>
     </table>
-  );
-};
-
-const Employee = ({ index, employee }) => {
-  const { name, position, salary, status } = employee;
-  return (
-    <tr>
-      <td>{index}</td>
-      <td>{name}</td>
-      <td>{position}</td>
-      <td>{status === "Active" ? salary : "N/A"}</td>
-      <td>{status}</td>
-    </tr>
   );
 };
 
